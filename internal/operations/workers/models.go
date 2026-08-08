76f031ee582e196a0b0d7f8655be04e8dcab4e30
Nigzmaf1a <Nigelkhasiani@gmail.com>
2026-08-08 19:50:00 +0300

    feat:patched workers model

--- internal/operations/workers/models.go
+++ internal/operations/workers/models.go
@@ -3,10 +3,12 @@ package workers
 import "time"
 
 type Worker struct {
-	UserId        int64     `json:"user_id"`
-	SectorID      int64     `json:"sector_id"`
-	RoleID        int64     `json:"role_id"`
-	OutletID      int64     `json:"outlet_id"`
-	WorkerPresent string    `json:"worker_present"`
-	SignInTime    time.Time `json:"sign_in_time"`
+	UserId         int64     `json:"user_id"`
+	SectorID       int64     `json:"sector_id"`
+	RoleID         int64     `json:"role_id"`
+	OutletID       int64     `json:"outlet_id"`
+	WorkerPresent  string    `json:"worker_present"`
+	SignInTime     time.Time `json:"sign_in_time"`
+	ShiftTime      time.Time `json:"shift_time"`
+	SignInLocation string    `json:"sign_in_location"`
 }
